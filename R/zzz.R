a5e7cc02046becbe66eaaa22423d10a42e0a3cc3
Michael Sevestre <michael@design2code.ca>
2019-07-26 13:42:00 -0400

    Prevent install on x86

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,11 @@
 .onLoad <- function(...) {
 
+  # Only for x64 bits packages. This is required to avoid error when package is being checked on CI for x86
+  is64 <- (.Machine$sizeof.pointer==8)
+  if(!is64){
+    return()
+  }
+#
   # Should only be called once
   initPackage()
 
