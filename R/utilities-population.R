6cab7ae6f42404536af16fbe66c1130a55ca5b39
Michael Sevestre <michael@design2code.ca>
2019-09-13 11:06:36 -0400

    WIP #37 load population parameters from CSV (#157)
    * WIP #37 load population parameters from CSV
    
    * Fixes #37 load population parameters from CSV

---
+++ R/utilities-population.R
@@ -0,0 +1,15 @@
+#' Loads a population from a csv file and returns the population.
+#'
+#' @param csvPopulationFile Full path of csv population file to load.
+#'
+#' @examples
+#' csvPath <- system.file("extdata", "pop.csv", package = "ospsuite")
+#'
+#' population <- loadPopulation(csvPath)
+#' @export
+loadPopulation <- function(csvPopulationFile) {
+  validateIsString(csvPopulationFile)
+  populationImporter <- getNetTask("PopulationImporter")
+  population <- rClr::clrCall(populationImporter, "ImportPopulation", csvPopulationFile)
+  Population$new(population)
+}
--- tests/testthat/helper-for-tests.R
+++ tests/testthat/helper-for-tests.R
@@ -1,12 +1,16 @@
-getSimulationFilePath <- function(simulationName) {
+
+getTestDataFilePath <- function(fileName) {
   dataPath <- file.path(getwd(), "..", "data", fsep = .Platform$file.sep)
-  simFile <- file.path(dataPath, paste0(simulationName, ".pkml"), fsep = .Platform$file.sep)
+  file.path(dataPath, fileName, fsep = .Platform$file.sep)
+}
+
+getSimulationFilePath <- function(simulationName) {
+  getTestDataFilePath(paste0(simulationName, ".pkml"))
 }
 
 # Helper function to load a model easily. In the test environment, we do not want to load from cache by default. Instead
 # new instances should be created unless specifically specified otherwise
 loadTestSimulation <- function(simulationName, loadFromCache = FALSE, addToCache = TRUE) {
-  dataPath <- file.path(getwd(), "..", "data", fsep = .Platform$file.sep)
   simFile <- getSimulationFilePath(simulationName)
   sim <- loadSimulation(simFile, loadFromCache = loadFromCache, addToCache = addToCache)
 }
