ae1efb68ea69b73776721e0d02f03d0739729e27
Michael Sevestre <michael@design2code.ca>
2020-01-21 20:06:19 +0000

    249 unifiy outputs (#259)
    * Fixes #249 unify output function
    
    * Fixes #111 default loadSimulation from cache

--- tests/dev/script-outputs.R
+++ tests/dev/script-outputs.R
@@ -4,14 +4,12 @@ sim <- loadSimulation("C:/projects/OSPSuite-R/tests/data/S1.pkml")
 individualResults <- runSimulation(sim)
 resultsPaths <- individualResults$allQuantityPaths
 
-tlf <- getOutputValuesTLF(individualResults)
+tlf <- getOutputValues(individualResults)
 print(tlf)
 
 population <- loadPopulation("C:/projects/OSPSuite-R/tests/data/pop_10.csv")
 populationResults <- runSimulation(sim, population)
 resultsPaths <- populationResults$allQuantityPaths
 
-results <- getOutputValues(populationResults, resultsPaths, individualIds = 1)
-
 path <- resultsPaths[[1]]
-results <- getOutputValuesTLF(populationResults, path, population = population, individualIds = c(0, 1))
+results <- getOutputValues(populationResults, path, population = population, individualIds = c(0, 1))
