dbc1489b866e097be735d1646bae3597d03af54d
Michael Sevestre <michael@design2code.ca>
2020-01-28 13:03:46 -0500

    Fixes #271 update table formula\

--- R/sensitivity-analysis-results.R
+++ R/sensitivity-analysis-results.R
@@ -35,7 +35,8 @@ SensitivityAnalysisResults <- R6::R6Class("SensitivityAnalysisResults",
     #' Returns the PKParameterSensitivity for a given pkParameter and output participating to a total sensitivity greater or equal to `totalSensitivityThreshold`
     #' @param pkParameterName Name of pkParameter for which sensitivity should be retrieved
     #' @param outputPath Path of the output for which the sensitivity should be retrieved
-    #' @param totalSensitivityThreshold A threshold of 0.9 means that only parameter participating to a total of 90 percent of the sensitivity would be returned
+    #' @param totalSensitivityThreshold Threshold used to filter out the most sensitive parameter. A threshold of 0.9 means that only
+    #' parameter participating to a total of 90 percent of the sensitivity would be returned. A value of 1 would return the sensitivity for all parameters.
     allPKParameterSensitivitiesFor = function(
                                                   pkParameterName,
                                                   outputPath,
