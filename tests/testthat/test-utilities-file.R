468a4687493126ab84af71e191fce7b61a7edb6d
Michael Sevestre <michael@design2code.ca>
2020-02-03 13:10:45 -0500

    Fixes #290 issues with normalizer

--- tests/testthat/test-utilities-file.R
+++ tests/testthat/test-utilities-file.R
@@ -7,3 +7,7 @@ test_that("Homogenizing an R path returns an R Path", {
 test_that("Homogenizing a windows path should return an R Path", {
   expect_equal(.homogenizePath("C:\\temp\\test.xml"), "C:/temp/test.xml")
 })
+
+test_that("Homogenizing a windows path ending with \ should return an R Path", {
+  expect_equal(.homogenizePath("C:\\temp\\"), "C:/temp")
+})
