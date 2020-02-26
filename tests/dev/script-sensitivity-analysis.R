18f69339fc95b07561dddd11d634c4f561a9c9bf
Michael Sevestre <michael@design2code.ca>
2020-02-26 11:30:46 -0500

    Update sensitvity script

--- tests/dev/script-sensitivity-analysis.R
+++ tests/dev/script-sensitivity-analysis.R
@@ -8,7 +8,7 @@ print(params)
 
 sensitivity <- SensitivityAnalysis$new(sim)
 sensitivity$addParameterPaths("Organism|Liver|Volume")
-dynamicPkParameter = DynamicPKParameter$new(name = "Test", standardPKParameter =  StandardPKParameter$Cmax)
+dynamicPkParameter = DynamicPKParameter$new(name = "Test", standardPKParameter =  StandardPKParameter$AucTend)
 print(dynamicPkParameter)
 sensitivity$addDynamicPKParameters(dynamicPkParameter)
 
@@ -28,6 +28,11 @@ importedResults <- importSensitivityAnalysisResultsFromCSV(simulation = sim, "in
 
 for (output in outputSelections$allOutputs) {
   pkSensitivities <- results$allPKParameterSensitivitiesFor(pkParameterName = "Test", outputPath = output$path)
+  for (pkSensitivity in pkSensitivities) {
+    print(pkSensitivity)
+  }
+
+  pkSensitivities <- results$allPKParameterSensitivitiesFor(pkParameterName = "AUC", outputPath = output$path)
   for (pkSensitivity in pkSensitivities) {
     print(pkSensitivity)
   }
