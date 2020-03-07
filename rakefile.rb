16037c34000502021b6a7f1fa82e6c50f94c8329
Michael Sevestre <michael@design2code.ca>
2020-03-07 09:58:16 -0500

    Fixes # linux build

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,7 @@
 require_relative 'scripts/copy-dependencies'
 require_relative 'scripts/utils'
 
-task :prepare_for_build,[:product_version] do |t, args|
+task :prepare_for_build, [:product_version] do |t, args|
   product_version = sanitized_version(args.product_version)
  
   copy_files_to_lib_folder
@@ -16,7 +16,58 @@ task :postclean do
   copy_files_to_lib_folder 
 end
 
+# This task is temporary until we have an automated linux buold
+task :create_linux_build, [:product_version, :build_dir] do |t, args|
+  product_version = sanitized_version(args.product_version)
+  build_dir = args.build_dir
+  tar_file_name = "ospsuite_#{product_version}.tar.gz"
+  # Tar file produced by the script
+  tar_file = File.join(build_dir, tar_file_name)
+
+  #unzip it in a temp folder
+  temp_dir = File.join(build_dir, "temp")
+  FileUtils.mkdir_p temp_dir
+  command_line = %W[xzf #{tar_file} -C #{temp_dir}]
+  Utils.run_cmd('tar', command_line)
+
+  #run nuget to get linux packages
+  nuget_restore 'linux'
+
+  ospsuite_dir = File.join(temp_dir,  'ospsuite')
+  inst_lib_diir = File.join(ospsuite_dir, 'inst', 'lib')
+
+  #Remove the windows dll that should be replace by linux binaries
+  delete_dll('OSPSuite.FuncParserNative', inst_lib_diir)
+  delete_dll('OSPSuite.SimModelNative', inst_lib_diir)
+  delete_dll('OSPSuite.SimModelSolver_CVODES', inst_lib_diir)
+
+  #Copy the linux binaries
+  copy_so('OSPSuite.FuncParser', inst_lib_diir)
+  copy_so('OSPSuite.SimModel', inst_lib_diir)
+  copy_so('OSPSuite.SimModelSolver_CVODES', inst_lib_diir)
+
+  #Recreate tar ball in temp file
+  temp_tar_file = File.join(temp_dir,  tar_file_name)
+  command_line = %W[czf #{temp_tar_file}  -C #{temp_dir} ospsuite]
+  Utils.run_cmd('tar', command_line)
+
+  #Last move new tar file and replace old tar file
+  FileUtils.copy_file(temp_tar_file, tar_file)
+end
+
 private
+
+def copy_so(file, target_dir)
+  native_folder = '/bin/native/x64/Release/'
+  copy_depdencies packages_dir, target_dir do
+    copy_files "#{file}.Ubuntu*/**/#{native_folder}", 'so'
+  end
+end
+
+def delete_dll(file, dir)
+  file_full_path = File.join(dir, "#{file}.dll")
+  File.delete(file_full_path)
+end
 
 def copy_files_to_lib_folder
   copy_packages_files
