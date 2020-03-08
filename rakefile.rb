78147ea5d5e4eb9130ab63d0b1d00f66531100c8
Michael Sevestre <michael@design2code.ca>
2020-03-08 14:29:15 -0400

    some more try

--- rakefile.rb
+++ rakefile.rb
@@ -31,24 +31,24 @@ task :create_linux_build, [:product_version, :build_dir] do |t, args|
   tar_file = File.join(build_dir, tar_file_name)
 
   #unzip it in a temp folder
-  #temp_dir = File.join(build_dir, "temp")
+  temp_dir = File.join(build_dir, "temp")
 #  FileUtils.mkdir_p temp_dir
-  temp_dir = "C:/temp/"
+#  temp_dir = "C:/temp/"
   FileUtils.mkdir_p temp_dir
 
   command_line = %W[xzf #{tar_file} -C #{temp_dir}]
   Utils.run_cmd('tar', command_line)
-  
-  command_line = %W[cvzf test.tar.gz  C:/temp/ospsuite/]
-  Utils.run_cmd('tar', command_line)
 
-  # ospsuite_dir = File.join(temp_dir,  'ospsuite')
-  # inst_lib_diir = File.join(ospsuite_dir, 'inst', 'lib')
+  ospsuite_dir = File.join(temp_dir,  'ospsuite')
+  inst_lib_diir = File.join(ospsuite_dir, 'inst', 'lib')
 
   #Remove the windows dll that should be replace by linux binaries
-  # delete_dll('OSPSuite.FuncParserNative', inst_lib_diir)
-  # delete_dll('OSPSuite.SimModelNative', inst_lib_diir)
-  # delete_dll('OSPSuite.SimModelSolver_CVODES', inst_lib_diir)
+  delete_dll('OSPSuite.FuncParserNative', inst_lib_diir)
+  delete_dll('OSPSuite.SimModelNative', inst_lib_diir)
+  delete_dll('OSPSuite.SimModelSolver_CVODES', inst_lib_diir)
+
+  command_line = %W[czf #{File.join(build_dir, tar_file_name)}   -C #{temp_dir} ospsuite]
+  Utils.run_cmd('tar', command_line)
 
   #Copy the linux binaries
   # copy_so('OSPSuite.FuncParser', inst_lib_diir)
@@ -67,6 +67,58 @@ task :create_linux_build, [:product_version, :build_dir] do |t, args|
   #Last move new tar file and replace old tar file
  # FileUtils.copy_file(temp_tar_file, tar_file)
 end
+
+# This task is temporary until we have an automated linux buold
+# task :create_linux_build, [:product_version, :build_dir] do |t, args|
+#   product_version = sanitized_version(args.product_version)
+#   build_dir = to_linux_path(args.build_dir)
+  
+#   #run nuget to get linux packages
+#   # nuget_restore 'linux'
+
+#   tar_file_name = "ospsuite_#{product_version}.tar.gz"
+
+  
+#   # Tar file produced by the script
+#   tar_file = File.join(build_dir, tar_file_name)
+
+#   #unzip it in a temp folder
+#   #temp_dir = File.join(build_dir, "temp")
+# #  FileUtils.mkdir_p temp_dir
+#   temp_dir = "C:/temp/"
+#   FileUtils.mkdir_p temp_dir
+
+#   command_line = %W[xzf #{tar_file} -C #{temp_dir}]
+#   Utils.run_cmd('tar', command_line)
+  
+#   command_line = %W[cvzf test.tar.gz  C:/temp/ospsuite/]
+#   Utils.run_cmd('tar', command_line)
+
+#   # ospsuite_dir = File.join(temp_dir,  'ospsuite')
+#   # inst_lib_diir = File.join(ospsuite_dir, 'inst', 'lib')
+
+#   #Remove the windows dll that should be replace by linux binaries
+#   # delete_dll('OSPSuite.FuncParserNative', inst_lib_diir)
+#   # delete_dll('OSPSuite.SimModelNative', inst_lib_diir)
+#   # delete_dll('OSPSuite.SimModelSolver_CVODES', inst_lib_diir)
+
+#   #Copy the linux binaries
+#   # copy_so('OSPSuite.FuncParser', inst_lib_diir)
+#   # copy_so('OSPSuite.SimModel', inst_lib_diir)
+#   # copy_so('OSPSuite.SimModelSolver_CVODES', inst_lib_diir)
+
+#   # puts "Temp Directory exists #{Dir.exists?(temp_dir)}"
+#   # puts "OSPSUITE  Directory exists #{Dir.exists?( File.join(temp_dir, "ospsuite"))}"
+
+#   #Recreate tar ball in temp file
+#   # temp_tar_file = File.join(temp_dir,  tar_file_name)
+# #  command_line = %W[czf #{temp_tar_file}  -C #{temp_dir} ospsuite]
+# # command_line = %W[cvzf #{tar_file_name}  #{temp_dir}]
+# # Utils.run_cmd('tar', command_line)
+
+#   #Last move new tar file and replace old tar file
+#  # FileUtils.copy_file(temp_tar_file, tar_file)
+# end
 
 private
 
