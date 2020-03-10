86ca707f51daec97a0734c60ed0d34c18f578204
Michael Sevestre <michael@design2code.ca>
2020-03-10 12:19:00 -0400

    Fixes #326 linux build (#330)
    * Fixes # linux build
    
    * Fixes # linux build
    
    * Change location of script => on success
    
    * Change location of script => on success
    
    * add verbose to understand what's going on
    
    * add verbose to understand what's going on
    
    * add verbose to understand what's going on
    
    * add verbose to understand what's going on
    
    * add verbose to understand what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * No idea what's going on
    
    * some more try
    
    * some more try
    
    * some more try
    
    * some more try
    
    * some more try
    
    * Also remove netstandard dlls
    
    * Try with cd in temp directory
    
    * Rename linux to ubuntu18
    
    * Try to split commands...WTF
    
    * Try to split commands...WTF

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,7 @@
 require_relative 'scripts/copy-dependencies'
 require_relative 'scripts/utils'
 
-task :prepare_for_build,[:product_version] do |t, args|
+task :prepare_for_build, [:product_version] do |t, args|
   product_version = sanitized_version(args.product_version)
  
   copy_files_to_lib_folder
@@ -16,7 +16,78 @@ task :postclean do
   copy_files_to_lib_folder 
 end
 
+# This task is temporary until we have an automated linux buold
+task :create_linux_build, [:product_version, :build_dir] do |t, args|
+  product_version = sanitized_version(args.product_version)
+  build_dir = args.build_dir
+  
+  linux_disto = 'ubuntu18'
+
+  #run nuget to get linux packages
+  nuget_restore linux_disto
+
+  tar_file_name = "ospsuite_#{product_version}.tar.gz"
+  
+  # Tar file produced by the script
+  tar_file = File.join(build_dir, tar_file_name)
+
+  #unzip it in a temp folder
+  temp_dir = File.join(build_dir, "temp")
+  FileUtils.mkdir_p temp_dir
+
+  command_line = %W[xzf #{tar_file} -C #{temp_dir}]
+  Utils.run_cmd('tar', command_line)
+
+  ospsuite_dir = File.join(temp_dir,  'ospsuite')
+  inst_lib_dir = File.join(ospsuite_dir, 'inst', 'lib')
+
+  #Remove the windows dll that should be replace by linux binaries
+  delete_dll('OSPSuite.FuncParserNative', inst_lib_dir)
+  delete_dll('OSPSuite.SimModelNative', inst_lib_dir)
+  delete_dll('OSPSuite.SimModelSolver_CVODES', inst_lib_dir)
+
+  #Copy the linux binaries
+  copy_so('OSPSuite.FuncParser', inst_lib_dir)
+  copy_so('OSPSuite.SimModel', inst_lib_dir)
+  copy_so('OSPSuite.SimModelSolver_CVODES', inst_lib_dir)
+
+  # zip_archive_name = "ospsuite_#{product_version}_#{linux_disto}.zip"
+  # zip_archive = File.join(build_dir,  zip_archive_name)
+  # command_line = %W[a #{zip_archive} #{ospsuite_dir}]
+  # zip command_line
+
+  Dir.chdir(temp_dir) do
+    tar_archive_name = "ospsuite_#{product_version}_#{linux_disto}.tar"
+    command_line = %W[cf #{tar_archive_name} ospsuite]
+    Utils.run_cmd('tar', command_line)
+
+    Utils.run_cmd('gzip', %W[#{tar_archive_name}])
+
+    FileUtils.mv("#{tar_archive_name}.gz", build_dir)
+  end
+end
+
+
 private
+
+def zip(command_line)
+  Utils.run_cmd('7z', command_line) 
+end
+
+def copy_so(file, target_dir)
+  native_folder = '/bin/native/x64/Release/'
+  copy_depdencies packages_dir, target_dir do
+    copy_files "#{file}.Ubuntu*/**/#{native_folder}", 'so'
+    copy_files "#{file}.Ubuntu*/**/netstandard*", 'dll'
+  end
+
+
+end
+
+def delete_dll(file, dir)
+  file_full_path = File.join(dir, "#{file}.dll")
+  File.delete(file_full_path)
+end
 
 def copy_files_to_lib_folder
   copy_packages_files
