d37e543032d6ef8d12f1685f3646d06fe1893db8
Michael Sevestre <michael@design2code.ca>
2020-04-17 14:36:49 -0400

    Add tests related to NA and NaN support

--- tests/dev/script-population.R
+++ tests/dev/script-population.R
@@ -1,8 +1,22 @@
 library(ospsuite)
 
 population <- loadPopulation("tests/data/pop_10.csv")
+sim <- loadSimulation("tests/data/S1.pkml")
+
+values <- c(1:10)*2.5;
+population$setParameterValues("Organism|Lung|Volume", values)
 
 population$getParameterValuesForIndividual(individualId = 0)
 ids <- population$allIndividualIds
 
 df <- populationAsDataFrame(population = population)
+
+
+res <- runSimulation(simulation = sim, population = population)
+
+
+values[2] <- NA
+values[4] <- NaN
+population$setParameterValues("Organism|Lung|Volume", values)
+res <- runSimulation(simulation = sim, population = population)
+
