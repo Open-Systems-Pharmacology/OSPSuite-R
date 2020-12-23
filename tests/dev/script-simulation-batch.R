28740c2222578602154834ce4cf53843f869e007
Michael Sevestre <michael@design2code.ca>
2020-12-23 16:22:35 -0500

    Fixes #444 batch simulation

--- tests/dev/script-simulation-batch.R
+++ tests/dev/script-simulation-batch.R
@@ -5,11 +5,16 @@ sim <- loadSimulation("tests/data/S1.pkml")
 parameter <- getParameter("Organism|Liver|Volume", sim)
 molecule <- getMolecule("Organism|Kidney|Intracellular|Caffeine", sim)
 
+
+# Create a simulation batch instance that will hold a reference to the simulation as well as manage the parameter and molecule to vary
 simulationBatch <- createSimulationBatch(sim, parametersOrPaths = c(parameter), moleculesOrPaths = molecule)
 
+# Run the simulation by updating parameters and initial values
+# Order of values is based on the order with which the parameters or initial values were defined
 res<- simulationBatch$run(parameterValues = 1.2, initialValues = c(2.5))
 values <-getOutputValues(res)
 data <- values$data$`Organism|ArterialBlood|Plasma|Caffeine|Plasma Unbound`
 
 
+# Update parameters and run again. The simulation is initialized only once
 res<- simulationBatch$run(parameterValues = 4, initialValues = c(4))
