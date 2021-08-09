299e8618619028097febc87f842499407eda1e93
Michael Sevestre <michael@design2code.ca>
2021-08-09 11:02:44 -0400

    Fixes #561 lloq data set (#574)
    * Fixes #561 lloq data set
    
    * Update after review PR

--- R/data-set.R
+++ R/data-set.R
@@ -118,16 +118,28 @@ DataSet <- R6::R6Class(
     },
     #' @field molWeight Molecular weight of the yValues in g/mol
     molWeight = function(value) {
-      dataInfo <- rClr::clrGet(private$.yColumn$ref, "DataInfo")
       if (missing(value)) {
-        molWeight <- rClr::clrGet(dataInfo, "MolWeight")
+        molWeight <- private$.yColumn$molWeight
         if (is.null(molWeight)) {
           return(NULL)
         }
         return(toUnit(quantityOrDimension = ospDimensions$`Molecular weight`, values = molWeight, targetUnit = ospUnits$`Molecular weight`$`g/mol`))
       }
 
-      rClr::clrSet(dataInfo, "MolWeight", toBaseUnit(quantityOrDimension = ospDimensions$`Molecular weight`, values = value, unit = ospUnits$`Molecular weight`$`g/mol`))
+      private$.yColumn$molWeight <- toBaseUnit(quantityOrDimension = ospDimensions$`Molecular weight`, values = value, unit = ospUnits$`Molecular weight`$`g/mol`)
+    },
+    #' @field LLOQ LLOQ Lower Limit Of Quantification.
+    #' Value in yUnit associated with the yValues
+    LLOQ = function(value) {
+      if (missing(value)) {
+        # Value in base unit
+        lloq <- private$.yColumn$LLOQ
+        if (is.null(lloq)) {
+          return(NULL)
+        }
+        return(toUnit(quantityOrDimension = private$.yColumn$dimension, values = lloq, targetUnit = private$.yColumn$displayUnit))
+      }
+      private$.yColumn$LLOQ <- toBaseUnit(quantityOrDimension = private$.yColumn$dimension, values = value, unit = private$.yColumn$displayUnit)
     },
 
     #' @field metaData Returns a named list of meta data defined for the data set.
@@ -209,6 +221,7 @@ DataSet <- R6::R6Class(
       private$printLine("Error type", self$yErrorType)
       private$printLine("Error unit", self$yErrorUnit)
       private$printLine("Molecular weight", self$molWeight)
+      private$printLine("LLOQ", self$LLOQ)
       private$printLine("Meta data")
       print(self$metaData)
       invisible(self)
