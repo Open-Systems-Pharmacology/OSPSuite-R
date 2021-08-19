f49b4597c046bf2846bad6e6898fcb916b3dc15a
Johanna Eitel <johanna.eitel@esqlabs.com>
2021-08-19 12:49:39 +0200

    Fixes #570
    plus tests test-utilities-data-set.R added for utilities-data-set.R

--- R/utilities-data-set.R
+++ R/utilities-data-set.R
@@ -1,8 +1,20 @@
-.makeDataFrameColumn <- function(dataSet, property) {
-  if (length(dataSet[[property]]) == 0) {
+.makeDataFrameColumn <- function(dataSet, property, metaDataName = NULL) {
+  # check length of entry for a certain property of this data set, i.e. if it exists
+  if (is.null(metaDataName)) {
+    len <- length(dataSet[[property]])
+  } else {
+    # len <- min(length(dataSet[[property]][[metaDataName]]), 1)
+    len <- length(dataSet[[property]][[metaDataName]])
+  }
+
+  if (len == 0) {
     rep(NA, length(dataSet$xValues))
-  } else if (length(dataSet[[property]]) == 1) {
-    rep(dataSet[[property]], length(dataSet$xValues))
+  } else if (len == 1) {
+    if (is.null(metaDataName)) {
+      rep(dataSet[[property]], length(dataSet$xValues))
+    } else {
+      rep(dataSet[[property]][[metaDataName]], length(dataSet$xValues))
+    }
   } else {
     dataSet[[property]]
   }
@@ -19,20 +31,32 @@ dataSetToDataFrame <- function(dataSets) {
   dataSets <- c(dataSets)
   validateIsOfType(dataSets, DataSet)
 
-  name <- unlist(mapply(makeDataFrameColumn, dataSets, "name"))
-  xUnit <- unlist(mapply(makeDataFrameColumn, dataSets, "xUnit"))
-  yUnit <- unlist(mapply(makeDataFrameColumn, dataSets, "yUnit"))
-  yErrorUnit <- unlist(mapply(makeDataFrameColumn, dataSets, "yErrorUnit"))
-  xDimension <- unlist(mapply(makeDataFrameColumn, dataSets, "xDimension"))
-  yDimension <- unlist(mapply(makeDataFrameColumn, dataSets, "yDimension"))
-  yErrorType <- unlist(mapply(makeDataFrameColumn, dataSets, "yErrorType"))
-  yMolWeight <- unlist(mapply(makeDataFrameColumn, dataSets, "molWeight"))
-  xValue <- unlist(mapply(makeDataFrameColumn, dataSets, "xValues"))
-  yValue <- unlist(mapply(makeDataFrameColumn, dataSets, "yValues"))
-  yErrorValues <- unlist(mapply(makeDataFrameColumn, dataSets, "yErrorValues"))
+  name <- unlist(mapply(.makeDataFrameColumn, dataSets, "name"))
+  xUnit <- unlist(mapply(.makeDataFrameColumn, dataSets, "xUnit"))
+  yUnit <- unlist(mapply(.makeDataFrameColumn, dataSets, "yUnit"))
+  yErrorUnit <- as.character(unlist(mapply(.makeDataFrameColumn, dataSets, "yErrorUnit")))
+  xDimension <- unlist(mapply(.makeDataFrameColumn, dataSets, "xDimension"))
+  yDimension <- unlist(mapply(.makeDataFrameColumn, dataSets, "yDimension"))
+  yErrorType <- as.character(unlist(mapply(.makeDataFrameColumn, dataSets, "yErrorType")))
+  yMolWeight <- unlist(mapply(.makeDataFrameColumn, dataSets, "molWeight"))
+  xValue <- unlist(mapply(.makeDataFrameColumn, dataSets, "xValues"))
+  yValue <- unlist(mapply(.makeDataFrameColumn, dataSets, "yValues"))
+  yErrorValues <- unlist(mapply(.makeDataFrameColumn, dataSets, "yErrorValues"))
 
-  return(data.frame(
+  df <- data.frame(
     name, xValue, yValue, yErrorValues, xDimension, xUnit, yDimension,
     yUnit, yErrorType, yErrorUnit, yMolWeight
-  ))
+  )
+
+  # get all names of meta data entries from all data sets
+  metaDataNames <- unique(unlist(sapply(dataSets, function(dataSets) {
+    return(names(dataSets[["metaData"]]))
+  })))
+  # add one column for each one
+  for (name in metaDataNames) {
+    col <- unlist(lapply(dataSets, .makeDataFrameColumn, "metaData", metaDataName = name))
+    df[[name]] <- col
+  }
+
+  return(df)
 }
