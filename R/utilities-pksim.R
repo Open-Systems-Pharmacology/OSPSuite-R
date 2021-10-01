343396523cbe8b554e27dfee74abded6a8d65916
Michael Sevestre <michael@design2code.ca>
2021-10-01 11:01:54 -0400

    Fixes #635 reorder vignettes

--- R/utilities-pksim.R
+++ R/utilities-pksim.R
@@ -8,6 +8,9 @@
 #' @import rClr
 #' @export
 initPKSim <- function(pksimFolderPath = NULL) {
+
+  pksimFolderPath <- "C:/projects/PK-Sim/src/PKSim/bin/Debug/net472"
+
   if (ospsuiteEnv$isPKSimLoaded) {
     return(invisible())
   }
