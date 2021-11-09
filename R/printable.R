a0d2e1740a0ff5342c10ed6a34eee082a98a1c66
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-11-09 14:17:31 +0100

    closes #655

--- R/printable.R
+++ R/printable.R
@@ -6,15 +6,16 @@ Printable <- R6::R6Class(
   "Printable",
   private = list(
     printLine = function(entry, value = NULL, addTab = TRUE) {
-      entries <- c(entry)
+      entries <- paste0(entry, ":", sep = "")
+
+      # helps to visually distinguish class name from its entries
       if (addTab) {
-        entries <- c("  ", entry)
+        entries <- c("  ", entries)
       }
-      entries <- c(entries, ": ", value)
 
+      entries <- c(entries, value)
       entries <- c(entries, "\n")
-      cat(entries, sep = "")
-
+      cat(entries, sep = " ")
       invisible(self)
     },
     printClass = function() {
