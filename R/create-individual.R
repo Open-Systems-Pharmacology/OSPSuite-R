ae48dc9cf9956297a3f207d978cd6b5b571dce3d
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-12-12 04:02:11 +0100

    just to trigger CI

--- R/create-individual.R
+++ R/create-individual.R
@@ -1,14 +1,16 @@
 #' Creates an individual using the PKSim Database
 #'
-#' @param individualCharacteristics Characteristics of the individual to create as an instance of `IndividualCharacteristics`
+#' @param individualCharacteristics Characteristics of the individual to create
+#'   as an instance of `IndividualCharacteristics`
 #'
 #' @return A list with three entries:
 #'  * `distributedParameters` containing the actual parameter values modified by the create individual algorithm.
 #'  * `derivedParameters` containing the parameter values modified indirectly by the algorithm. Those parameters are typically formula parameters.
 #'  * `seed` containing the seed value used to generate random values
 #'
-#' @note When updating a simulation with the value for a new individual, only use the `distributedParameters` to ensure that you do not override
-#' formula parameters.
+#' @note When updating a simulation with the value for a new individual, only
+#'   use the `distributedParameters` to ensure that you do not override formula
+#'   parameters.
 #'
 #' @export
 createIndividual <- function(individualCharacteristics) {
