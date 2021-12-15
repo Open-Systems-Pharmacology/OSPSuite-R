0a0e5e347711baf479743906919e53afc34e522a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2021-12-15 04:15:42 +0100

    use helper to tidy dataframe

--- R/simulationResultsToDataFrame.R
+++ R/simulationResultsToDataFrame.R
@@ -14,7 +14,6 @@
 #'
 #' # convert to a dataframe
 #' simulationResultsToDataFrame(results)
-#'
 #' @return
 #'
 #' SimulationResults object as data.frame with columns IndividualId, Time, paths,
@@ -36,18 +35,18 @@ simulationResultsToDataFrame <- function(simulationResults,
   )
 
   # convert data to long format with a new column for paths
-  df_data <- simList$data %>%
-    tidyr::pivot_longer(
-      cols = -c("IndividualId", "Time"),
-      names_to = "paths",
-      values_to = "simulationValues"
-    )
+  df_data <- .convert_to_long(
+    simList$data,
+    cols = setdiff(names(simList$data), c("IndividualId", "Time")),
+    names_to = "paths",
+    values_to = "simulationValues"
+  )
 
   # extract units and dimensions for paths in a separate dataframe
   # leave out time units and dimensions since it is not a path
   df_meta <- purrr::imap_dfr(
     .x  = simList$metaData,
-    .f  = ~as.data.frame(.x, row.names = NULL),
+    .f  = ~ as.data.frame(.x, row.names = NULL),
     .id = "paths"
   ) %>%
     dplyr::filter(paths != "Time")
@@ -63,4 +62,80 @@ simulationResultsToDataFrame <- function(simulationResults,
 
   # return the combined dataframe
   return(df)
+}
+
+# simplified version of
+# https://github.com/easystats/datawizard/blob/master/R/data_reshape.R
+# IP is author on this package
+
+.convert_to_long <- function(data,
+                             cols = "all",
+                             names_to = "Name",
+                             values_to = "Value") {
+  if (inherits(data, "tbl_df")) {
+    tbl_input <- TRUE
+    data <- as.data.frame(data)
+  } else {
+    tbl_input <- FALSE
+  }
+
+
+  if (is.character(cols) && length(cols) == 1) {
+    if (cols == "all") {
+      # If all, take all
+      cols <- names(data)
+    } else {
+      # Surely, a regex
+      cols <- grep(cols, names(data), value = TRUE)
+    }
+  }
+
+  # If numeric, surely the index of the cols
+  if (is.numeric(cols)) {
+    cols <- names(data)[cols]
+  }
+
+  # Compatibility with tidyr
+  if (names_to != names_to) names_to <- names_to
+
+  # save attribute of each variable
+  variable_attr <- lapply(data, attributes)
+
+  # Create Index column as needed by reshape
+  data[["_Row"]] <- as.numeric(row.names(data))
+
+  # Reshape
+  long <- stats::reshape(data,
+    varying = cols,
+    idvar = "_Row",
+    v.names = values_to,
+    timevar = names_to,
+    direction = "long"
+  )
+
+  # Sort the dataframe (to match pivot_longer's output)
+  long <- long[order(long[["_Row"]], long[[names_to]]), ]
+
+  # Remove or rename the row index
+  long[["_Row"]] <- NULL
+
+  # Re-insert col names as levels
+  long[[names_to]] <- cols[long[[names_to]]]
+
+  # Reset row names
+  row.names(long) <- NULL
+
+  # Remove reshape attributes
+  attributes(long)$reshapeLong <- NULL
+
+  # add back attributes where possible
+  for (i in colnames(long)) {
+    attributes(long[[i]]) <- variable_attr[[i]]
+  }
+
+  if (isTRUE(tbl_input)) {
+    class(long) <- c("tbl_df", "tbl", "data.frame")
+  }
+
+  long
 }
