210a39e59bbbf1f6199dc767a43df3b7212e3547
PavelBal <pavel.balazki@gmail.com>
2022-01-03 11:10:22 +0100

    Adjust documentation

--- R/utilities.R
+++ R/utilities.R
@@ -43,12 +43,27 @@ netEnumName <- function(enumType, enumValue) {
 }
 
 
-#' Create a DataImporterConfiguration object for a XLS file
+#' Create a DataImporterConfiguration object for an XLS sheet
+#' @details The function tries to parse the structure of the excel sheet and
+#' creates a default configuration for this sheet. It is advised to check the
+#' configuration and adjust if necessary before using with `loadDataSetsFromExcel()`.
 #'
 #' @param filePath Path to XLS file
-#' @param sheet optional - name of the sheet
+#' @param sheet optional - name of the sheet. If no sheet is specified, the first
+#' sheet of the XLS file is used.
 #'
-#' @return DataImporterConfiguration object for XLS file
+#' @return `DataImporterConfiguration` object for XLS file to be used in
+#' `loadDataSetsFromExcel()`.
+#' @examples
+#' xlsFilePath <- "../CompiledDataSet.xlsx"
+#' importerConfiguration <- createConfigurationForFile(xlsFilePath)
+#' importerConfiguration$sheets <- "TestSheet_1"
+#'
+#' dataSets <- loadDataSetsFromExcel(
+#'   xlsFilePath = xlsFilePath,
+#'   importerConfiguration = importerConfiguration,
+#'   importAllSheets = TRUE
+#' )
 #' @export
 createConfigurationForFile = function(filePath, sheet = NULL) {
   # warum sind aenderungen, z.b. unit nicht in ref?????
