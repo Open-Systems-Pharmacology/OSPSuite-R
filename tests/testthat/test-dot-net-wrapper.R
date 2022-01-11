995accb8250020ecd8b948d0cd6ecfd4a37d119c
Michael Sevestre <michael@design2code.ca>
2022-01-11 17:29:54 +0100

    Add tests

---
+++ tests/testthat/test-dot-net-wrapper.R
@@ -0,0 +1,11 @@
+context("DotNetWrapper")
+
+sim <- loadTestSimulation("S1")
+
+test_that("It can retrieve the reference for an object loaded", {
+  expect_false(is.null(sim$ref))
+})
+
+test_that("It throws an error when trying to overwrite the ref value", {
+  expect_error(sim$ref <- "toto")
+})
