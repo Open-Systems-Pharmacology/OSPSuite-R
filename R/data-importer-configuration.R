0b4817297f6dd078e73ec624c1e209f763532668
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-02 10:01:16 +0100

    add missing curly braces

--- R/data-importer-configuration.R
+++ R/data-importer-configuration.R
@@ -220,9 +220,9 @@ DataImporterConfiguration <- R6::R6Class(
       rClr::clrCall(private$.dataImporterTask, "SetAllLoadedSheet", self$ref, value)
     },
     #' @field namingPattern Regular expression used for naming of loaded data sets.
-    #' Words between curly brackets (e.g. `Group Id` will be replaced by the value
-    #' in the corresponding column. Further keywords are `Source` for the file name
-    #' and `Sheet` for sheet name.
+    #' Words between curly brackets (e.g. `{Group Id}` will be replaced by the value
+    #' in the corresponding column. Further keywords are `{Source}` for the file name
+    #' and `{Sheet}` for sheet name.
     namingPattern = function(value) {
       if (missing(value)) {
         pattern <- rClr::clrGet(self$ref, "NamingConventions")
