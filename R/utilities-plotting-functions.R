b5f3d4a59eb60b949d6dd82a67a76f4a983b464a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-14 13:29:20 +0100

    skeleton for concentration profile plot
    Can't proceed before https://github.com/Open-Systems-Pharmacology/TLF-Library/issues/224 is resolved.

---
+++ R/utilities-plotting-functions.R
@@ -0,0 +1,59 @@
+#' Concentration time profile plot
+#'
+#' @param dataCombined A `DataCombined` object.
+#' @inheritParams tlf::useTheme
+#' @inheritParams PlotConfiguration$new
+#'
+#' @import tlf
+#'
+#' @export
+plotIndividualTimeProfile <- function(dataCombined,
+                                      theme = NULL,
+                                      title = NULL,
+                                      subtitle = NULL,
+                                      xlabel = NULL,
+                                      ylabel = NULL,
+                                      legendTitle = NULL) {
+  validateIsOfType(dataCombined, "DataCombined")
+
+  df <- dataCombined$toDataFrame()
+
+  obsData <- dplyr::filter(df, dataType == "observed")
+  simData <- dplyr::filter(df, dataType == "simulated")
+
+  # TODO: feels a bit hacky
+  #
+  # Can this be fixed in `DataCombined$toDataFrame()` itself?
+  #
+  # "Doing unit conversion by hand is very suspicious. Both simulated and
+  # observed should be converted to the same data so that they can be compared"
+  # - Michael
+  if (unique(obsData$yUnit) == "%" && unique(simData$yUnit) == "") {
+    simData <- simData %>% dplyr::mutate(yValues = yValues * 100)
+  }
+
+  useTheme(loadThemeFromJson(system.file("themes", "ospsuiteTheme.json", package = "ospsuite")))
+
+  plotTimeProfile(
+    data = as.data.frame(simData),
+    dataMapping = TimeProfileDataMapping$new(
+      x = "xValues",
+      y = "yValues",
+      group = "name"
+    ),
+    observedData = as.data.frame(obsData),
+    observedDataMapping = ObservedDataMapping$new(
+      x = "xValues",
+      y = "yValues",
+      group = "group",
+      uncertainty = "yErrorValues"
+    ),
+    plotConfiguration = TimeProfilePlotConfiguration$new(
+      title = title,
+      subtitle = subtitle,
+      xlabel = xlabel,
+      ylabel = ylabel,
+      legendTitle = legendTitle
+    )
+  )
+}
