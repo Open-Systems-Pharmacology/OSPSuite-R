dbcf10fbe609b4efdfc44bd016ec914cd9e2cb8d
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-28 10:08:51 +0200

    change API

--- R/plotIndividualTimeProfile.R
+++ R/plotIndividualTimeProfile.R
@@ -4,26 +4,15 @@
 #' @param tlfTheme A path to JSON file containing
 #'   [`Theme`](https://www.open-systems-pharmacology.org/TLF-Library/reference/Theme.html)
 #'    object for `{tlf}` library.
-#' @param xAxisScale,yAxisScale A character/string that decides transformations
-#'   for axes (Default: `"lin"` (for continuous linear scale)).
-#'   Other available options are:
-#'   - `"log"` (for continuous log10 scale (i.e., with base 10))
-#'   - `"ln"` (for continuous natural logarithm (ln) scale (i.e., with base *e*))
-#'   - `"sqrt"` (for continusous square root scale)
-#' @param title,subtitle,xlabel,ylabel,legendTitle A character/string defining
-#'   plot title, subtitle, axes labels, legend title, respectively.
+#' @param individualTimeProfilePlotConfiguration A `PlotConfiguration` object.
+#'   For more, see:
+#'   <https://www.open-systems-pharmacology.org/TLF-Library/articles/plot-configuration.html>.
 #'
 #' @import tlf
 #'
 #' @export
 plotIndividualTimeProfile <- function(dataCombined,
-                                      xAxisScale = "lin",
-                                      yAxisScale = "lin",
-                                      title = NULL,
-                                      subtitle = NULL,
-                                      xlabel = NULL,
-                                      ylabel = NULL,
-                                      legendTitle = NULL,
+                                      individualTimeProfilePlotConfiguration = TimeProfilePlotConfiguration$new(),
                                       tlfTheme = NULL) {
   validateIsOfType(dataCombined, "DataCombined")
 
@@ -32,47 +21,40 @@ plotIndividualTimeProfile <- function(dataCombined,
   obsData <- dplyr::filter(df, dataType == "observed")
   simData <- dplyr::filter(df, dataType == "simulated")
 
-  # TODO: feels a bit hacky
-  #
-  # Can this be fixed in `DataCombined$toDataFrame()` itself?
-  #
-  # "Doing unit conversion by hand is very suspicious. Both simulated and
-  # observed should be converted to the same data so that they can be compared"
-  # - Michael
+  # TODO: remove once `unitConverter()` function is available
   if (unique(obsData$yUnit) == "%" && unique(simData$yUnit) == "") {
     simData <- simData %>% dplyr::mutate(yValues = yValues * 100)
   }
-
 
   tlfTheme <- tlfTheme %||% system.file("themes", "ospsuiteTLFTheme.json", package = "ospsuite")
 
   useTheme(loadThemeFromJson(tlfTheme))
 
-  ospsuiteTimeProfilePlotConfiguration <- TimeProfilePlotConfiguration$new(
-    title = title,
-    subtitle = subtitle,
-    xlabel = xlabel,
-    ylabel = ylabel,
-    legendTitle = legendTitle
-  )
-
-  ospsuiteTimeProfilePlotConfiguration$xAxis$scale <- xAxisScale
-  ospsuiteTimeProfilePlotConfiguration$yAxis$scale <- yAxisScale
+  # ospsuiteTimeProfilePlotConfiguration <- TimeProfilePlotConfiguration$new(
+  #   title = title,
+  #   subtitle = subtitle,
+  #   xlabel = xlabel,
+  #   ylabel = ylabel,
+  #   legendTitle = legendTitle
+  # )
+  #
+  # ospsuiteTimeProfilePlotConfiguration$xAxis$scale <- xAxisScale
+  # ospsuiteTimeProfilePlotConfiguration$yAxis$scale <- yAxisScale
 
   plotTimeProfile(
     data = as.data.frame(simData),
     dataMapping = TimeProfileDataMapping$new(
       x = "xValues",
       y = "yValues",
-      group = "name"
+      group = "group"
     ),
     observedData = as.data.frame(obsData),
     observedDataMapping = ObservedDataMapping$new(
       x = "xValues",
       y = "yValues",
       group = "group",
-      uncertainty = "yErrorValues"
+      error = "yErrorValues"
     ),
-    plotConfiguration = ospsuiteTimeProfilePlotConfiguration
+    plotConfiguration = individualTimeProfilePlotConfiguration
   )
 }
