a749ea4c54dfe27a102793f423b737433d5e8d36
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-06 14:28:11 +0200

    test

--- tests/testthat/test-plot-grid.R
+++ tests/testthat/test-plot-grid.R
@@ -25,4 +25,27 @@ test_that("plots grid is rendered correctly", {
     title = "plotGrid works as expected",
     fig = plotGrid(plotGridObj)
   )
+
+  library(ggplot2)
+
+  # `{tlf}` plot
+  set.seed(123)
+  boxData <- data.frame(x = c(rep("A", 500), rep("B", 500)), y = rlnorm(1000))
+  p1 <- plotBoxWhisker(data = boxData, dataMapping = BoxWhiskerDataMapping$new(x = "x", y = "y"))
+
+  # custom `{ggplot2}` plot
+  p2 <- ggplot(mtcars, aes(wt, mpg)) +
+    geom_point()
+
+  plotGridObj2 <- createPlotGridConfiguration(
+    plotList = list(p1, p2),
+    nColumns = 1L,
+    tagLevels = "i"
+  )
+
+  set.seed(123)
+  vdiffr::expect_doppelganger(
+    title = "plotGrid allows mixing of tlf and custom ggplot objects",
+    fig = plotGrid(plotGridObj2)
+  )
 })
