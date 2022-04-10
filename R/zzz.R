3a0ba38d368202f9a168b90d766946c69457cc0a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-10 12:12:18 +0200

    exclude zzz from code coverage computation

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
+# nocov start
 .onLoad <- function(...) {
 
-  # Only for x64 bits packages. This is required to avoid error when package is being checked on CI for x86
+  # Only for x64 bits packages.
+  # This is required to avoid error when package is being checked on CI for x86
   is64 <- (.Machine$sizeof.pointer == 8)
   if (!is64) {
     return()
@@ -8,3 +10,4 @@
 
   initPackage()
 }
+# nocov end
