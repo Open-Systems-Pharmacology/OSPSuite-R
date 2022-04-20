01bc3a22f5a5a2e07e651864ba1062914cdd310a
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-20 17:49:53 +0200

    minor improvements for NET task docs and code (#880)
    * minor improvements for NET task docs and code
    
    * address code review comments

--- R/get-net-task.R
+++ R/get-net-task.R
@@ -1,19 +1,30 @@
-#' Returns an instance of the specified .NET Task
+#' Returns an instance of the specified `.NET` Task
 #'
-#' @param taskName The name of the task to retrieve (without the Get)
+#' @param taskName The name of the task to retrieve (**without** `Get` prefix).
 #'
-#' @return An instance of the Task
+#' @return
 #'
-#' @details
-#' simulationLoader <- getNetTask("SimulationLoader")
+#' - `getNetTask()` returns an instance of of the specified `.NET` task.
+#' - `getContainerTask()` returns a reference to the container task for
+#' optimization purposes only.
+#'
+#' @examples
+#'
+#' ospsuite:::getNetTask("SimulationRunner")
+#'
+#' ospsuite:::getContainerTask()
+#'
 #' @keywords internal
 getNetTask <- function(taskName) {
   rClr::clrCallStatic("OSPSuite.R.Api", paste0("Get", taskName))
 }
 
+#' @rdname getNetTask
+#' @keywords internal
 getContainerTask <- function() {
   if (is.null(ospsuiteEnv$containerTask)) {
     ospsuiteEnv$containerTask <- getNetTask("ContainerTask")
   }
+
   ospsuiteEnv$containerTask
 }
