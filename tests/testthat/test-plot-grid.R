27a9389bc9e4fe20fde27ce04b19c80fb6911b3c
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-21 19:01:01 +0200

    add example for custom + tlf instead of a test

--- tests/testthat/test-plot-grid.R
+++ tests/testthat/test-plot-grid.R
@@ -36,29 +36,4 @@ test_that("plots grid is rendered correctly", {
     title = "plotGrid works as expected",
     fig = plotGrid(plotGridObj)
   )
-
-  # needed to create a custom plot
-  library(ggplot2)
-
-  # `{tlf}` plot
-  set.seed(123)
-  p1 <- tlf::plotBoxWhisker(mtcars,
-    dataMapping = tlf::BoxWhiskerDataMapping$new(x = "am", y = "wt"), outliers = FALSE
-  )
-
-  # custom `{ggplot2}` plot
-  set.seed(123)
-  p2 <- ggplot(mtcars, aes(wt, mpg)) +
-    geom_point()
-
-  plotGridObj2 <- PlotGridConfiguration$new(list(p1, p2))
-
-  plotGridObj2$nColumns <- 1L
-  plotGridObj2$tagLevels <- "i"
-
-  set.seed(123)
-  vdiffr::expect_doppelganger(
-    title = "plotGrid allows mixing of tlf and custom ggplot objects",
-    fig = plotGrid(plotGridObj2)
-  )
 })
