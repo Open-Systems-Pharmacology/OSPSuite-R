20b8654679134782d897c7f8afadcb098f745fa5
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-26 17:16:02 +0200

    fixes #889

--- R/messages.R
+++ R/messages.R
@@ -5,3 +5,8 @@ messages <- ospsuite.utils::messages
 messages$errorWrongPopulation <- function(species, population) {
   paste0("Could not find population '", population, "' for species '", species, "'")
 }
+
+messages$errorOneOfNameAndPathMustBeSpecified <- function() {
+  "One of the `parameterName` or `parameterPath` must be specified, but not both."
+}
+
