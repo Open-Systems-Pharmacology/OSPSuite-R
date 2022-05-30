7c9cd9c7d286763d7fbd8661f966ca57b7e23132
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-05-30 11:10:29 +0200

    Move path explorer content to utilities simulation
    Closes #800
    
    - [ ] Prefix internal functions with `.`
    - [ ] Move code to `utilities-simulation.R`
    - [ ] Move tests to `test-utilities-simulation.R`

--- tests/testthat/test-utilities-simulation.R
+++ tests/testthat/test-utilities-simulation.R
@@ -399,3 +399,22 @@ test_that("It returns the correct paths of the state variables", {
   stateVariablePaths <- getAllStateVariablesPaths(simulation = sim)
   expect_equal(length(stateVariablePaths), 5)
 })
+
+context("getSimulationTree")
+
+test_that("it can explore a simulation by path", {
+  simPath <- getSimulationFilePath("simple")
+  tree <- getSimulationTree(simPath)
+
+  path <- tree$Organism$Liver$Volume$path
+  expect_equal(path, "Organism|Liver|Volume")
+})
+
+
+test_that("it can explore a simulation by instance", {
+  sim <- loadTestSimulation("simple")
+  tree <- getSimulationTree(sim)
+
+  path <- tree$Organism$Liver$Volume$path
+  expect_equal(path, "Organism|Liver|Volume")
+})
