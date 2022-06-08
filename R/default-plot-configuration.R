Open-Systems-Pharmacology/OSPSuite-R

commit 08d8d1098d76a032eacdfdb6ad57270b6970486b

Fix axes tick labels (#995)
Closes #981

R/default-plot-configuration.R
@@ -232,7 +232,7 @@ DefaultPlotConfiguration <- R6::R6Class(
     xAxisLimits = NULL,
     xAxisScale = tlf::Scaling$lin,
     xAxisTicks = NULL,
-    xAxisTicksLabels = NULL,
+    xAxisTicksLabels = identity,
     xAxisLabelTicksSize = NULL,
     xAxisLabelTicksColor = "black",
     xAxisLabelTicksFontFamily = "",
@@ -245,7 +245,7 @@ DefaultPlotConfiguration <- R6::R6Class(
     yAxisLimits = NULL,
     yAxisScale = tlf::Scaling$lin,
     yAxisTicks = NULL,
-    yAxisTicksLabels = NULL,
+    yAxisTicksLabels = identity,
     yAxisLabelTicksSize = NULL,
     yAxisLabelTicksColor = "black",
     yAxisLabelTicksFontFamily = "",

tests/testthat/_snaps/plot-individual-time-profile/default-plot-both.svg
@@ -362,11 +362,11 @@
 </g>
 <g clip-path='url(#cpMC4wMHw3MjAuMDB8MC4wMHw1NzYuMDA=)'>
 <polyline points='41.57,544.93 41.57,5.48 ' style='stroke-width: 1.07; stroke: #333333; stroke-linecap: butt;' />
-<text x='36.64' y='523.85' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>0.00</text>
+<text x='36.64' y='523.85' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='5.56px' lengthAdjust='spacingAndGlyphs'>0</text>
 <text x='36.64' y='401.25' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>0.25</text>
-<text x='36.64' y='278.65' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>0.50</text>
+<text x='36.64' y='278.65' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='13.90px' lengthAdjust='spacingAndGlyphs'>0.5</text>
 <text x='36.64' y='156.04' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>0.75</text>
-<text x='36.64' y='33.44' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>1.00</text>
+<text x='36.64' y='33.44' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='5.56px' lengthAdjust='spacingAndGlyphs'>1</text>
 <polyline points='38.83,520.41 41.57,520.41 ' style='stroke-width: 1.07; stroke: #333333; stroke-linecap: butt;' />
 <polyline points='38.83,397.81 41.57,397.81 ' style='stroke-width: 1.07; stroke: #333333; stroke-linecap: butt;' />
 <polyline points='38.83,275.21 41.57,275.21 ' style='stroke-width: 1.07; stroke: #333333; stroke-linecap: butt;' />

tests/testthat/_snaps/plot-individual-time-profile/default-plot-simulated.svg
@@ -106,11 +106,11 @@
 </g>
 <g clip-path='url(#cpMC4wMHw3MjAuMDB8MC4wMHw1NzYuMDA=)'>
 <polyline points='41.57,544.93 41.57,5.48 ' style='stroke-width: 1.07; stroke: #333333; stroke-linecap: butt;' />
-<text x='36.64' y='523.85' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>0.00</text>
+<text x='36.64' y='523.85' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='5.56px' lengthAdjust='spacingAndGlyphs'>0</text>
 <text x='36.64' y='401.25' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>0.25</text>
-<text x='36.64' y='278.64' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>0.50</text>
+<text x='36.64' y='278.64' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='13.90px' lengthAdjust='spacingAndGlyphs'>0.5</text>
 <text x='36.64' y='156.04' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>0.75</text>
-<text x='36.64' y='33.43' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='19.46px' lengthAdjust='spacingAndGlyphs'>1.00</text>
+<text x='36.64' y='33.43' text-anchor='end' style='font-size: 10.00px; fill: #333333; font-family: sans;' textLength='5.56px' lengthAdjust='spacingAndGlyphs'>1</text>
 <polyline points='38.83,520.41 41.57,520.41 ' style='stroke-width: 1.07; stroke: #333333; stroke-linecap: butt;' />
 <polyline points='38.83,397.81 41.57,397.81 ' style='stroke-width: 1.07; stroke: #333333; stroke-linecap: butt;' />
 <polyline points='38.83,275.20 41.57,275.20 ' style='stroke-width: 1.07; stroke: #333333; stroke-linecap: butt;' />

tests/testthat/test-plot-individual-time-profile.R
@@ -64,6 +64,7 @@ test_that("It respects custom plot configuration", {
   myPlotConfiguration$legendPosition <- tlf::LegendPositions$outsideRight
   myPlotConfiguration$pointsColor <- tlf::ColorMaps$default
   myPlotConfiguration$linesLinetype <- names(tlf::Linetypes)
+  myPlotConfiguration$yAxisScale <- tlf::Scaling$log
 
   set.seed(123)
   vdiffr::expect_doppelganger(

commit c63ae797364c77ba1228854a2220648c3bf1b528

fix documentation issue
This issue was introduced in https://github.com/Open-Systems-Pharmacology/OSPSuite-R/pull/995

R/default-plot-configuration.R
@@ -111,8 +111,8 @@
 #'   Available options can be seen using `tlf::LegendPositions` list.
 #' @field legendTitleSize,legendTitleColor,legendTitleFontFamily,legendTitleFontFace,legendTitleAngle,legendTitleAlign Aesthetic properties for the legend title.
 #' @field legendCaptionSize,legendCaptionColor,legendCaptionFontFamily,legendCaptionFontFace,legendCaptionAngle,legendCaptionAlign Aesthetic properties for the legend caption.
-#' @field xAxisTicksLabels,xAxisLabelTicksSize,xAxisLabelTicksColor,xAxisLabelTicksFontFamily,xAxisLabelTicksFontFace,xAxisLabelTicksAngle,xAxisLabelTicksAlign Aesthetic properties for the x-axis label.
-#' @field yAxisTicksLabels,yAxisLabelTicksSize,yAxisLabelTicksColor,yAxisLabelTicksFontFamily,yAxisLabelTicksFontFace,yAxisLabelTicksAngle,yAxisLabelTicksAlign Aesthetic properties for the y-axis label.
+#' @field xAxisLabelTicksSize,xAxisLabelTicksColor,xAxisLabelTicksFontFamily,xAxisLabelTicksFontFace,xAxisLabelTicksAngle,xAxisLabelTicksAlign Aesthetic properties for the x-axis label.
+#' @field yAxisLabelTicksSize,yAxisLabelTicksColor,yAxisLabelTicksFontFamily,yAxisLabelTicksFontFace,yAxisLabelTicksAngle,yAxisLabelTicksAlign Aesthetic properties for the y-axis label.
 #' @field xAxisLimits,yAxisLimits A numeric vector of axis limits for the x-and
 #'   y-axis, respectively.
 #' @field xAxisTicks,yAxisTicks A numeric vector or a function defining where to
@@ -232,7 +232,13 @@ DefaultPlotConfiguration <- R6::R6Class(
     xAxisLimits = NULL,
     xAxisScale = NULL,
     xAxisTicks = NULL,
-    xAxisTicksLabels = identity,
+    #' @description
+    #' Transformation function to convert X-axis tick labels. The default is
+    #' identity function. That is, the values will be shown as they are in the
+    #' form of labels.
+    #'
+    #' @param x A value.
+    xAxisTicksLabels = function(x) x,
     xAxisLabelTicksSize = NULL,
     xAxisLabelTicksColor = "black",
     xAxisLabelTicksFontFamily = "",
@@ -245,7 +251,13 @@ DefaultPlotConfiguration <- R6::R6Class(
     yAxisLimits = NULL,
     yAxisScale = NULL,
     yAxisTicks = NULL,
-    yAxisTicksLabels = identity,
+    #' @description
+    #' Transformation function to convert Y-axis tick labels. The default is
+    #' identity function. That is, the values will be shown as they are in the
+    #' form of labels.
+    #'
+    #' @param x A value.
+    yAxisTicksLabels = function(x) x,
     yAxisLabelTicksSize = NULL,
     yAxisLabelTicksColor = "black",
     yAxisLabelTicksFontFamily = "",

man/DefaultPlotConfiguration.Rd
@@ -147,9 +147,9 @@ Available options can be seen using \code{tlf::LegendPositions} list.}
 
 \item{\code{legendCaptionSize, legendCaptionColor, legendCaptionFontFamily, legendCaptionFontFace, legendCaptionAngle, legendCaptionAlign}}{Aesthetic properties for the legend caption.}
 
-\item{\code{xAxisTicksLabels, xAxisLabelTicksSize, xAxisLabelTicksColor, xAxisLabelTicksFontFamily, xAxisLabelTicksFontFace, xAxisLabelTicksAngle, xAxisLabelTicksAlign}}{Aesthetic properties for the x-axis label.}
+\item{\code{xAxisLabelTicksSize, xAxisLabelTicksColor, xAxisLabelTicksFontFamily, xAxisLabelTicksFontFace, xAxisLabelTicksAngle, xAxisLabelTicksAlign}}{Aesthetic properties for the x-axis label.}
 
-\item{\code{yAxisTicksLabels, yAxisLabelTicksSize, yAxisLabelTicksColor, yAxisLabelTicksFontFamily, yAxisLabelTicksFontFace, yAxisLabelTicksAngle, yAxisLabelTicksAlign}}{Aesthetic properties for the y-axis label.}
+\item{\code{yAxisLabelTicksSize, yAxisLabelTicksColor, yAxisLabelTicksFontFamily, yAxisLabelTicksFontFace, yAxisLabelTicksAngle, yAxisLabelTicksAlign}}{Aesthetic properties for the y-axis label.}
 
 \item{\code{xAxisLimits, yAxisLimits}}{A numeric vector of axis limits for the x-and
 y-axis, respectively.}
@@ -207,9 +207,9 @@ label, y-axis label, plot legend, watermark, respectively.}
 
 \item{\code{legendCaptionSize, legendCaptionColor, legendCaptionFontFamily, legendCaptionFontFace, legendCaptionAngle, legendCaptionAlign}}{Aesthetic properties for the legend caption.}
 
-\item{\code{xAxisTicksLabels, xAxisLabelTicksSize, xAxisLabelTicksColor, xAxisLabelTicksFontFamily, xAxisLabelTicksFontFace, xAxisLabelTicksAngle, xAxisLabelTicksAlign}}{Aesthetic properties for the x-axis label.}
+\item{\code{xAxisLabelTicksSize, xAxisLabelTicksColor, xAxisLabelTicksFontFamily, xAxisLabelTicksFontFace, xAxisLabelTicksAngle, xAxisLabelTicksAlign}}{Aesthetic properties for the x-axis label.}
 
-\item{\code{yAxisTicksLabels, yAxisLabelTicksSize, yAxisLabelTicksColor, yAxisLabelTicksFontFamily, yAxisLabelTicksFontFace, yAxisLabelTicksAngle, yAxisLabelTicksAlign}}{Aesthetic properties for the y-axis label.}
+\item{\code{yAxisLabelTicksSize, yAxisLabelTicksColor, yAxisLabelTicksFontFamily, yAxisLabelTicksFontFace, yAxisLabelTicksAngle, yAxisLabelTicksAlign}}{Aesthetic properties for the y-axis label.}
 
 \item{\code{xAxisLimits, yAxisLimits}}{A numeric vector of axis limits for the x-and
 y-axis, respectively.}
@@ -247,7 +247,47 @@ Available options can be seen using \code{tlf::Scaling} list.}
 \section{Methods}{
 \subsection{Public methods}{
 \itemize{
+\item \href{#method-DefaultPlotConfiguration-xAxisTicksLabels}{\code{DefaultPlotConfiguration$xAxisTicksLabels()}}
+\item \href{#method-DefaultPlotConfiguration-yAxisTicksLabels}{\code{DefaultPlotConfiguration$yAxisTicksLabels()}}
 \item \href{#method-DefaultPlotConfiguration-clone}{\code{DefaultPlotConfiguration$clone()}}
+}
+}
+\if{html}{\out{<hr>}}
+\if{html}{\out{<a id="method-DefaultPlotConfiguration-xAxisTicksLabels"></a>}}
+\if{latex}{\out{\hypertarget{method-DefaultPlotConfiguration-xAxisTicksLabels}{}}}
+\subsection{Method \code{xAxisTicksLabels()}}{
+Transformation function to convert X-axis tick labels. The default is
+identity function. That is, the values will be shown as they are in the
+form of labels.
+\subsection{Usage}{
+\if{html}{\out{<div class="r">}}\preformatted{DefaultPlotConfiguration$xAxisTicksLabels(x)}\if{html}{\out{</div>}}
+}
+
+\subsection{Arguments}{
+\if{html}{\out{<div class="arguments">}}
+\describe{
+\item{\code{x}}{A value.}
+}
+\if{html}{\out{</div>}}
+}
+}
+\if{html}{\out{<hr>}}
+\if{html}{\out{<a id="method-DefaultPlotConfiguration-yAxisTicksLabels"></a>}}
+\if{latex}{\out{\hypertarget{method-DefaultPlotConfiguration-yAxisTicksLabels}{}}}
+\subsection{Method \code{yAxisTicksLabels()}}{
+Transformation function to convert Y-axis tick labels. The default is
+identity function. That is, the values will be shown as they are in the
+form of labels.
+\subsection{Usage}{
+\if{html}{\out{<div class="r">}}\preformatted{DefaultPlotConfiguration$yAxisTicksLabels(x)}\if{html}{\out{</div>}}
+}
+
+\subsection{Arguments}{
+\if{html}{\out{<div class="arguments">}}
+\describe{
+\item{\code{x}}{A value.}
+}
+\if{html}{\out{</div>}}
 }
 }
 \if{html}{\out{<hr>}}

man/dot-createObsVsPredData.Rd
@@ -4,12 +4,15 @@
 \alias{.createObsVsPredData}
 \title{Created observed versus simulated paired data}
 \usage{
-.createObsVsPredData(data)
+.createObsVsPredData(data, tolerance = 0.001)
 }
 \arguments{
 \item{data}{A data frame from \code{DataCombined$toDataFrame()}, which has been
 further tidied using \code{.removeUnpairableDatasets()} and then
 \code{.unitConverter()} functions.}
+
+\item{tolerance}{Tolerance of comparison for observed and simulated time
+points.}
 }
 \description{
 Created observed versus simulated paired data

man/plotObservedVsSimulated.Rd
@@ -7,8 +7,8 @@
 plotObservedVsSimulated(
   dataCombined,
   defaultPlotConfiguration = NULL,
-  smoother = NULL,
-  lines = c(-0.2, 0, 0.2)
+  smoother = "lm",
+  foldDistance = c(0, 1.5, 2)
 )
 }
 \arguments{
@@ -22,6 +22,8 @@ an \code{R6} class object that defines plot properties.}
 \item \code{"loess"} for loess regression
 \item \code{"lm"} for linear regression
 }}
+
+\item{foldDistance}{A vector for plotting lines at required fold differences.}
 }
 \description{
 Observed versus predicted/simulated scatter plot