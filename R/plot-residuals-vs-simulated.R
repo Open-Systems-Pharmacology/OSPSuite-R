7da4cdf6d90dc6ee652eeef82cbc934ae7a26f76
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-09 14:33:26 +0200

    Add examples to the manual
    Closes #1065

--- R/plot-residuals-vs-simulated.R
+++ R/plot-residuals-vs-simulated.R
@@ -9,7 +9,41 @@
 #'
 #' @examples
 #'
-#' # TODO: add example
+#' # simulated data
+#' simFilePath <- system.file("extdata", "Aciclovir.pkml", package = "ospsuite")
+#' sim <- loadSimulation(simFilePath)
+#' simResults <- runSimulation(sim)
+#' outputPath <- "Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)"
+#'
+#' # observed data
+#' obsData <- lapply(
+#'   c("ObsDataAciclovir_1.pkml", "ObsDataAciclovir_2.pkml", "ObsDataAciclovir_3.pkml"),
+#'   function(x) loadDataSetFromPKML(system.file("extdata", x, package = "ospsuite"))
+#' )
+#' names(obsData) <- lapply(obsData, function(x) x$name)
+#'
+#'
+#' # Create a new instance of `DataCombined` class
+#' myDataCombined <- DataCombined$new()
+#'
+#' # Add simulated results
+#' myDataCombined$addSimulationResults(
+#'   simulationResults = simResults,
+#'   quantitiesOrPaths = outputPath,
+#'   groups = "Aciclovir PVB"
+#' )
+#'
+#' # Add observed data set
+#' myDataCombined$addDataSets(obsData$`Vergin 1995.Iv`, groups = "Aciclovir PVB")
+#'
+#' # Create a new instance of `DefaultPlotConfiguration` class
+#' myPlotConfiguration <- DefaultPlotConfiguration$new()
+#' myPlotConfiguration$title <- "My Plot Title"
+#' myPlotConfiguration$subtitle <- "My Plot Subtitle"
+#' myPlotConfiguration$caption <- "My Sources"
+#'
+#' # plot
+#' plotResidualsVsSimulated(myDataCombined, myPlotConfiguration)
 #'
 #' @export
 plotResidualsVsSimulated <- function(dataCombined,
