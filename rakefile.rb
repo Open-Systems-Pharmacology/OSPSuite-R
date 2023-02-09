b98d5ee758405a497bb07019e49fb54a3b966edf
Pavel Balazki <pavel.balazki@gmail.com>
2023-02-09 12:52:06 +0100

    Fix missing argument for download_file in rakefile.rb

--- rakefile.rb
+++ rakefile.rb
@@ -17,19 +17,19 @@ task :prepare_for_build, [:build_version, :pksim_branch] do |t, args|
 end
 
 # TEMP
-# This creates a folder with a minimal set of PK-Sim dlls 
-# that are necessary to run OSPSuite-R without any further 
-# PK-Sim installation. 
+# This creates a folder with a minimal set of PK-Sim dlls
+# that are necessary to run OSPSuite-R without any further
+# PK-Sim installation.
 task :download_portable, [:pksim_branch] do |t, args|
   args.with_defaults(:pksim_branch => 'develop')
   download_pksim_portable(args.pksim_branch)
 end
 
-task :postclean do 
+task :postclean do
   os = 'win'
   clear_folders
   nuget_restore os
-  copy_files_to_lib_folder 
+  copy_files_to_lib_folder
 end
 
 # This task is temporary until we have an automated linux build
@@ -98,7 +98,7 @@ def install_pksim(branch)
   appveyor_project_name = 'pk-sim'
   uri = "https://ci.appveyor.com/api/projects/#{APPVEYOR_ACCOUNT_NAME}/#{appveyor_project_name}/artifacts/#{file_name}?branch=#{branch}"
   download_dir = File.join(temp_dir, appveyor_project_name)
-  zip_package = download_file(appveyor_project_name, file_name, uri)
+  zip_package = download_file(appveyor_project_name, file_name, uri, download_dir)
   msi_package = unzip_package(zip_package)
   # MSI installer only works with \\ style separator
   msi_package = msi_package.split('/').join('\\')
@@ -108,7 +108,7 @@ def install_pksim(branch)
   puts "Installation done.".light_blue
 end
 
-def download_file(project_name, file_name, uri, download_dir) 
+def download_file(project_name, file_name, uri, download_dir)
   FileUtils.mkdir_p download_dir
   file = File.join(download_dir, file_name)
   puts "Downloading #{file_name} from #{uri} under #{file}".light_blue
@@ -126,19 +126,19 @@ def download_pksim_portable(branch)
 
   download_dir = "C:/projects/ospsuite-r"
   portable_zip_package = download_file(appveyor_project_name, portable_file_name, portable_uri, download_dir)
-  
+
   Utils.run_cmd('7z', %W[e #{portable_zip_package}])
 
   pksim_minimal_dir = "C:/projects/ospsuite-r/pksim_minimal.zip"
   command_line = %W[
-    a 
-    #{pksim_minimal_dir} 
-    Castle.Core.dll 
-    Castle.Windsor.dll 
-    DevExpress.Data*.dll 
-    FluentNHibernate.dll 
-    MarkdownLog.dll 
-    Newtonsoft.Json.dll 
+    a
+    #{pksim_minimal_dir}
+    Castle.Core.dll
+    Castle.Windsor.dll
+    DevExpress.Data*.dll
+    FluentNHibernate.dll
+    MarkdownLog.dll
+    Newtonsoft.Json.dll
     Newtonsoft.Json.Schema.dll
     NHibernate.dll
     OSPSuite.Assets.Images.dll
@@ -169,7 +169,7 @@ def unzip_package(package_full_path)
   artifact_name = ''
   Dir.glob(File.join(unzip_dir, '*.msi')) do |x|
     artifact_name = x
-  end 
+  end
   artifact_name
 end
 
@@ -250,7 +250,7 @@ end
 
 def description_file
   File.join(solution_dir,'DESCRIPTION')
-end 
+end
 
 def temp_dir
   "C:/temp"
