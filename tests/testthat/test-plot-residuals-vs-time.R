cb3b243bf4741e923839291f7ba8aff93169e583
unknown <felix.mil@esqlabs.com>
2023-03-02 16:45:57 +0100

    Fix copy/paste error

--- tests/testthat/test-plot-residuals-vs-time.R
+++ tests/testthat/test-plot-residuals-vs-time.R
@@ -162,6 +162,6 @@ test_that("scaling residuals argument works with log", {
   set.seed(123)
   vdiffr::expect_doppelganger(
     title = "log-residuals",
-    fig = plotResidualsVsSimulated(myCombDat,scaling = "log")
+    fig = plotResidualsVsTime(myCombDat,scaling = "log")
   )
 })
