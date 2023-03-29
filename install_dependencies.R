0cc06f5bd1a369b9eb82c17b813355e34b1c7393
Pavel Balazki <pavel.balazki@gmail.com>
2023-03-29 17:03:36 +0200

    Add {sysfonts} as package

--- install_dependencies.R
+++ install_dependencies.R
@@ -1,4 +1,4 @@
-install.packages(c('testthat', 'vdiffr', 'spelling','covr', 'showtext'), repos = 'http://cran.us.r-project.org')
+install.packages(c('testthat', 'vdiffr', 'spelling','covr', 'showtext', 'sysfonts'), repos = 'http://cran.us.r-project.org')
 install.packages('https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v0.9.1/rClr_0.9.1.zip', repos = NULL, type = 'binary')
 download.file('https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/ospsuite-rutils/artifacts/ospsuite.utils.zip?pr=false', destfile = 'ospsuite.utils.zip', mode='wb'); install.packages('ospsuite.utils.zip', repos = NULL, type = 'binary')
 download.file('https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/tlf-library/artifacts/tlf.zip?pr=false', destfile = 'tlf.zip', mode='wb');  install.packages('tlf.zip', repos = NULL, type = 'binary')
