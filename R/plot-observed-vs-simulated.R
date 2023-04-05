1d72743444c8513d13c53f684738a17cfecf3078
Felix MIL <felix.mil@esqlabs.com>
2023-04-05 10:07:42 +0200

    Set default foldDistance to NULL in obs-vs-pred plot + Remove confusion between identity line & folds lines + Adapt previous test accordingly + Add new test cases + devtools::document()

--- R/plot-observed-vs-simulated.R
+++ R/plot-observed-vs-simulated.R
@@ -2,11 +2,13 @@
 #'
 #' @inheritParams plotIndividualTimeProfile
 #' @param foldDistance A vector for plotting lines at required fold distances
-#'   The vector can include only fold distance values `>1`. An
-#'   `x`-fold distance is defined as all simulated values within the range
-#'   between `x`-fold (depicted by the upper fold range line) and `1/x`-fold
-#'   (depicted by the lower fold range line) of observed values. The identity
-#'   line can be interpreted as the `1`-fold range.
+#' around the identity line (`x=y`). Set to NULL (default) to only draw identity
+#' line.
+#' The vector can include only fold distance values `>1`. An `x`-fold distance
+#' is defined as all simulated values within the range between `x`-fold
+#' (depicted by the upper fold range line) and `1/x`-fold
+#' (depicted by the lower fold range line) of observed values. The identity line
+#'  can be interpreted as the `1`-fold range.
 #'
 #' @import tlf
 #'
@@ -51,7 +53,7 @@
 #' @export
 plotObservedVsSimulated <- function(dataCombined,
                                     defaultPlotConfiguration = NULL,
-                                    foldDistance = 2) {
+                                    foldDistance = NULL) {
   # validation -----------------------------
 
   defaultPlotConfiguration <- .validateDefaultPlotConfiguration(defaultPlotConfiguration)
@@ -75,22 +77,20 @@ plotObservedVsSimulated <- function(dataCombined,
       obsVsPredPlotConfiguration$yAxis$scale == tlf::Scaling$identity
   )
 
-  # The argument `foldDistance` should only include fold values different from
-  # the default value, which must always be present.
-  #
-  # The default value depends on the scale:
-  #
+  # Identity Line (x=y) definition. Its value depends on the scale:
   # - For linear scale: `1`
   # - For logarithmic scale: `0`
-  defaultFoldDistance <- ifelse(is_any_scale_linear, 0, 1)
+  identity <- ifelse(is_any_scale_linear, 0, 1)
 
   # foldDistance should be above 1
   if (any(foldDistance <= 1)) {
     stop(messages$plotObservedVsSimulatedWrongFoldDistance("foldDistance", foldDistance))
   }
 
-  if (!any(dplyr::near(defaultFoldDistance, foldDistance))) {
-    foldDistance <- c(defaultFoldDistance, foldDistance)
+  # The argument `foldDistance` should only include fold values different from
+  # the abline, which must always be present.
+  if (!any(dplyr::near(identity, foldDistance))) {
+    foldDistance <- c(identity, foldDistance)
   }
 
   if (is_any_scale_linear && !is.null(foldDistance)) {
