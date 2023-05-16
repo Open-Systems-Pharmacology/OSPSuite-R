a865a39b2e5268b26f8995e1c2716dfdda5160e4
Felix MIL <felix.mil@esqlabs.com>
2023-05-16 09:49:25 +0200

    Create unicoße.pkml on the fly during the test to prevent errors during installation on non utf8 system

--- tests/testthat/test-unicode-support.R
+++ tests/testthat/test-unicode-support.R
@@ -7,9 +7,16 @@ test_that("It can load a simulation whose name contains unicode characters", {
   # is already corrupted - so no chance for ospsuite-r to load it properly
   # The same happens if e.g. the `{ospsuite}` sources are just zipped under Windows and unzipped under Linux - the file name becomes corrupted
 
+  # The unicoße file is created on the fly for this file impact other aspects of the package like installation from github on non utf8 systems.
+
+  file.copy(from = getTestDataFilePath("unicode.pkml"),
+            to = getTestDataFilePath("unicoße.pkml"))
+
   sim <- loadTestSimulation("unicoße")
   expect_false(is.null(sim))
 
   param <- getParameter("Organism|Liver|paré", sim)
   expect_false(is.null(param))
+
+  file.remove(getTestDataFilePath("unicoße.pkml"))
 })
