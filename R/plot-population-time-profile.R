20e08e0f393958f1b2acfc246fd204459796f3ac
Felix MIL <felix.mil@esqlabs.com>
2023-06-28 15:15:28 +0200

    Make `quantiles` still a valid argument of plotPopulationTimeProfile

--- R/plot-population-time-profile.R
+++ R/plot-population-time-profile.R
@@ -28,6 +28,9 @@
 #' # plot with other quantiles
 #' plotPopulationTimeProfile(myDataComb, probs = c(0.1, 0.5, 0.9))
 #'
+#' # Previous quantiles argument still works
+#' plotPopulationTimeProfile(myDataComb, quantiles = c(0.1, 0.5, 0.9))
+#'
 #' # plot with arithmetic mean
 #' plotPopulationTimeProfile(myDataComb,
 #'                           aggregation = "arithmetic")
@@ -43,5 +46,26 @@ plotPopulationTimeProfile <- function(dataCombined,
                                       aggregation = "quantiles",
                                       ...) {
 
-  .plotTimeProfile(dataCombined, defaultPlotConfiguration, aggregation, ...)
+  # Make previous "quantiles" argument deprecated but still working
+  lifecycle::deprecate_warn(when = "11.2.0",
+                            what = "plotPopulationTimeProfile(quantiles)",
+                            with = "plotPopulationTimeProfile(...)",
+                            details = "Use ... to pass extra parameters to
+                            aggregating functions.
+                            `probs` for `stats::quantile` or `n` for the number
+                            of standard deviation to add below and above the
+                            average for `arithmetic` or `geometric`."
+  )
+
+  args <- list(...)
+  # set probs argument
+  if ("quantiles" %in% names(args)) {
+    args$probs <- args$quantiles
+  }
+
+  do.call(.plotTimeProfile,
+          args = list(dataCombined = dataCombined,
+                      defaultPlotConfiguration = defaultPlotConfiguration,
+                      aggregation = aggregation,
+                      ... = args))
 }
