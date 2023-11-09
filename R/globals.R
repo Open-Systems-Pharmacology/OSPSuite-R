415edc4c3a78c37db48fd89213eecd24ec56b3ba
Felix MIL <felix.mil@esqlabs.com>
2023-11-09 16:38:49 +0100

    Remove warning about false positive "undefined variables"

---
+++ R/globals.R
@@ -0,0 +1,27 @@
+# These variables are set to NULL to avoid R CMD Check warning
+# 'no visible global function definition for ..."
+
+# defining global variables and functions to appease R CMD Check
+
+utils::globalVariables(
+  names = c(
+    ".",
+    ".rowidInternal",
+    "Time",
+    "dataType",
+    "group",
+    "name",
+    "paths",
+    "residualValues",
+    "unitFrequency",
+    "xValues",
+    "yValues",
+    "yErrorValues",
+    "yValuesObserved",
+    "yValuesSimulated"
+  ),
+  package = "ospsuite",
+  add = FALSE
+)
+
+
