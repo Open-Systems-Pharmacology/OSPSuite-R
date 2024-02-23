b4faee1db0b6bf23bd33ed169b25fba25c2a2532
PavelBal <pavel.balazki@gmail.com>
2024-02-23 17:05:41 +0100

    - Added workaround for https://github.com/Open-Systems-Pharmacology/OSPSuite-R/issues/1365 - Added internal function `.setEndSimulationTime()`. - Steady state simulation do not clear output intervals but sets the end time to the provided. Otherwise, some events might not fire.

--- tests/testthat/test-output-schema.R
+++ tests/testthat/test-output-schema.R
@@ -31,3 +31,46 @@ test_that("it can remove interval references", {
 test_that("It can print output schema", {
   expect_error(capture.output(outputSchema$print()), NA)
 })
+
+test_that("it correctly returns the end time", {
+  # Before adding a time point outside of the time intervals
+  expect_equal(outputSchema$endTime, 1440)
+  # Add a time point outside of the time intervals
+  outputSchema$addTimePoints(1441)
+  expect_equal(outputSchema$endTime, 1441)
+})
+
+test_that(".setEndSimulationTime adds a new time point outside of the current schema", {
+  .setEndSimulationTime(sim, 1442)
+  expect_equal(outputSchema$endTime, 1442)
+})
+
+
+test_that(".setEndSimulationTime adds a new time point smaller than the current end time without time points", {
+  sim <- loadTestSimulation("S1")
+  outputSchema <- sim$outputSchema
+  .setEndSimulationTime(sim, 1430)
+
+  expect_equal(outputSchema$intervals[[1]]$startTime$value, 0)
+  expect_equal(outputSchema$intervals[[1]]$endTime$value, 120)
+  expect_equal(outputSchema$intervals[[2]]$startTime$value, 120)
+  expect_equal(outputSchema$intervals[[2]]$endTime$value, 1430)
+  expect_equal(outputSchema$timePoints, 1430)
+
+  expect_equal(outputSchema$endTime, 1430)
+})
+
+test_that(".setEndSimulationTime adds a new time point smaller than the current end time with time points", {
+  sim <- loadTestSimulation("S1")
+  outputSchema <- sim$outputSchema
+  outputSchema$addTimePoints(c(1, 2, 121))
+  .setEndSimulationTime(sim, 119)
+
+  expect_equal(outputSchema$intervals[[1]]$startTime$value, 0)
+  expect_equal(outputSchema$intervals[[1]]$endTime$value, 119)
+  # Second interval is removed
+  expect_length(outputSchema$intervals, 1)
+  expect_equal(outputSchema$timePoints, c(1, 2, 119))
+
+  expect_equal(outputSchema$endTime, 119)
+})
