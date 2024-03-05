c4ea6f624b2a004061603be3eb101a9cc021a6a1
PavelBal <pavel.balazki@gmail.com>
2024-03-05 12:38:06 +0100

    Moved some wrappers to NetObject in rSharp

--- R/dot-net-wrapper.R
+++ R/dot-net-wrapper.R
@@ -6,10 +6,10 @@
 #' sim <- loadSimulation(system.file("extdata", "Aciclovir.pkml", package = "ospsuite"))
 #'
 #' # looking at a reference to `.NET` simulation object
-#' sim$ref
+#' sim$pointer
 #'
 #' # create a new instance of `DotNetWrapper` class using this reference
-#' DotNetWrapper$new(sim$ref)
+#' DotNetWrapper$new(sim$pointer)
 #'
 #' @export
 DotNetWrapper <- R6::R6Class(
@@ -17,63 +17,36 @@ DotNetWrapper <- R6::R6Class(
   inherit = NetObject,
   cloneable = FALSE,
   private = list(
-    # Simple way to wrap a get; .NET Read-Only property
-    wrapReadOnlyProperty = function(propertyName, value) {
-      if (missing(value)) {
-        rSharp::clrGet(self$ref, propertyName)
-      } else {
-        private$throwPropertyIsReadonly(propertyName)
-      }
-    },
     wrapExtensionMethod = function(typename, methodName, propertyName, value) {
       if (missing(value)) {
-        rSharp::callStatic(typename, methodName, self$ref)
+        rSharp::callStatic(typename, methodName, self$pointer)
       } else {
-        private$throwPropertyIsReadonly(propertyName)
+        private$.throwPropertyIsReadonly(propertyName)
       }
     },
     wrapExtensionMethodCached = function(typename, methodName, propertyName, cachedValue, value) {
       if (missing(value)) {
         if (is.null(cachedValue)) {
-          return(rSharp::callStatic(typename, methodName, self$ref))
+          return(rSharp::callStatic(typename, methodName, self$pointer))
         }
         return(cachedValue)
       } else {
-        private$throwPropertyIsReadonly(propertyName)
+        private$.throwPropertyIsReadonly(propertyName)
       }
     },
     readOnlyProperty = function(propertyName, value, returnValue) {
       if (missing(value)) {
         returnValue
       } else {
-        private$throwPropertyIsReadonly(propertyName)
+        private$.throwPropertyIsReadonly(propertyName)
       }
     },
-    wrapIntegerProperty = function(propertyName, value) {
-      # Special method needed because of double to int conversion issues between R and `.NET`
-      if (missing(value)) {
-        rSharp::clrGet(self$ref, propertyName)
-      } else {
-        rSharp::clrSet(self$ref, propertyName, as.integer(value))
-      }
-    },
-    wrapNullableIntegerProperty = function(propertyName, value) {
-      if (missing(value)) {
-        return(rSharp::clrGet(self$ref, propertyName))
-      }
-      if (is.null(value)) {
-        return()
-      } else {
-        rSharp::clrSet(self$ref, propertyName, as.integer(value))
-      }
-    },
-
     # Special method needed because Index are 0-based in `.NET` but 1-based in R
     wrapIndexProperty = function(propertyName, value) {
       if (missing(value)) {
-        rSharp::clrGet(self$ref, propertyName) + 1
+        self$get(propertyName) + 1
       } else {
-        rSharp::clrSet(self$ref, propertyName, as.integer(value - 1))
+        self$set(propertyName, value - 1, asInteger = TRUE)
       }
     },
 
@@ -85,22 +58,14 @@ DotNetWrapper <- R6::R6Class(
     # on whether R vector (for `value`) is of length 1 or higher.
     wrapVectorProperty = function(propertyNameSingular, propertyNamePlural, value, returnPropertyName) {
       if (missing(value)) {
-        rSharp::clrGet(self$ref, returnPropertyName)
+        self$get(returnPropertyName)
       } else {
         if (length(value) == 1L) {
-          rSharp::clrSet(self$ref, propertyNameSingular, value)
+          self$set(propertyNameSingular, value)
         } else {
-          rSharp::clrSet(self$ref, propertyNamePlural, value)
+          self$set(propertyNamePlural, value)
         }
       }
-    },
-    throwPropertyIsReadonly = function(propertyName) {
-      stop(messages$errorPropertyReadOnly(propertyName))
-    },
-
-    # maybe dispose should be called to if available
-    finalize = function() {
-      private$.ref <- NULL
     }
   )
 )
