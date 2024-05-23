a05bf4a4455f6b471f1bd18a2e055bd57ce93492
Felix MIL <felix.mil@esqlabs.com>
2024-05-23 09:32:32 +0200

    Tweak test to avoid writing file in package directory

--- tests/testthat/test-utilities-simulation.R
+++ tests/testthat/test-utilities-simulation.R
@@ -504,11 +504,12 @@ test_that("It calculates steady-state for multiple simulations, multiple steadyS
 })
 
 test_that("`exportSteadyStateToXLS` generates excel file with correct sheets", {
-  withr::with_tempdir(
+  withr::with_tempfile("resultsXLSPath",
     code = {
       simFilePath <- system.file("extdata", "Aciclovir.pkml", package = "ospsuite")
       sim <- loadSimulation(simFilePath)
-      wb <- exportSteadyStateToXLS(sim)
+      wb <- exportSteadyStateToXLS(sim, resultsXLSPath = resultsXLSPath)
+      expect_true(file.exists(resultsXLSPath))
       expect_equal(wb$sheet_names, c("Molecules", "Parameters"))
     }
   )
