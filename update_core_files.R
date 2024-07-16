f951541e7cc8ba965a02955deff4a2b91737a5aa
Felix MIL <felix.mil@esqlabs.com>
2024-07-16 12:40:40 +0200

    Add script to update core files

---
+++ update_core_files.R
@@ -0,0 +1,5 @@
+download.file("https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/pk-sim/artifacts/pk-sim-r-dependencies.zip",
+              destfile = "inst/lib/pk-sim-r-dependencies.zip")
+
+unzip("inst/lib/pk-sim-r-dependencies.zip")
+
