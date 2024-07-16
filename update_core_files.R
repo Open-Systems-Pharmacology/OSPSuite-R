674faac6a02257cddd15244d9c3d5be07a676780
Felix MIL <felix.mil@esqlabs.com>
2024-07-16 12:44:33 +0200

    fix unzip path

--- update_core_files.R
+++ update_core_files.R
@@ -1,5 +1,5 @@
 download.file("https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/pk-sim/artifacts/pk-sim-r-dependencies.zip",
               destfile = "inst/lib/pk-sim-r-dependencies.zip")
 
-unzip("inst/lib/pk-sim-r-dependencies.zip")
+unzip("inst/lib/pk-sim-r-dependencies.zip", exdir = "inst/lib")
 
