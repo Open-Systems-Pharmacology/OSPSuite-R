52bc0d1891a84d0fffc68b95ecbbf9831387a4d9
Felix MIL <dev@felixmil.com>
2025-02-25 16:26:51 +0100

    Add logic for loading dylib files on macos

--- R/init-package.R
+++ R/init-package.R
@@ -2,6 +2,7 @@
 #'
 #' This will be called once when the package is loaded
 #'
+#'
 #' @import rSharp
 #' @keywords internal
 .initPackage <- function() {
@@ -18,12 +19,37 @@
     ))
   }
 
-  # Load the .so binary files on unix
-  if (.Platform$OS.type == "unix") {
+
+if (.Platform$OS.type == "unix") {
+
+  if (Sys.info()[['sysname']] == "Linux"){
+    # Load the .so binary files on unix/linux
     for (soFile in list.files(system.file("lib", package = ospsuiteEnv$packageName), pattern = "\\.so$", full.names = TRUE)) {
       dyn.load(soFile)
     }
+  } else if(Sys.info()[['sysname']] == "Darwin"){
+
+    # if ARM64 architecture, rename *Arm64.dylib files into *.dylib
+    if (Sys.info()[['machine']] == "arm64") {
+      for (arm64File in list.files(system.file("lib", package = ospsuiteEnv$packageName), pattern = "\\.dylib$", full.names = TRUE)) {
+        file.copy(arm64File, gsub(".Arm64.dylib", ".dylib", arm64File))
+      }
+    }
+    # if x64 architecture, rename *x64.dylib files to *.dylib
+    else if (Sys.info()[['machine']] == "x86_64") {
+      for (x64File in list.files(system.file("lib", package = ospsuiteEnv$packageName), pattern = "\\.dylib$", full.names = TRUE)) {
+        file.copy(x64File, gsub(".x64.dylib", ".dylib",  x64File))
+      }
+    }
+
+    # Load the .dylib (not x64.dylib nor Arm64.dylib) binary files on mac
+    dylibFile <- list.files(system.file("lib", package = ospsuiteEnv$packageName), pattern = ".dylib$", full.names = TRUE)
+    filtered_dylibFile <- dylibFile[!grepl("\\.(x64|Arm64)\\.dylib$", dylibFile)]
+    for (dylibFile in filtered_dylibFile) {
+      dyn.load(dylibFile)
+    }
   }
+}
 
   rSharp::loadAssembly(filePathFor("OSPSuite.R.dll"))
   # Initialize once
