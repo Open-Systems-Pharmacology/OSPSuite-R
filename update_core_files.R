4fbe07a3784a9ba8a4b00161e05a503aab4e8652
Felix MIL <dev@felixmil.com>
2025-09-24 11:49:58 +0200

    Unversion files that are dynamically generated on package load. + rename System.Data.SQLite.dll that is pulled by update-core-files to System.Data.SQLite.others.dll to differenciate from the mac build

--- update_core_files.R
+++ update_core_files.R
@@ -15,3 +15,15 @@ unzip(
 )
 
 file.remove("pk-sim-r-dependencies_artefact.zip", "pk-sim-r-dependencies.zip")
+
+# Rename System.Data.SQLite.dll to System.Data.SQLite.others.dll
+# This file will be renamed back to System.Data.SQLite.dll on package load
+sqlite_dll_path <- file.path("inst/lib", "System.Data.SQLite.dll")
+sqlite_others_path <- file.path("inst/lib", "System.Data.SQLite.others.dll")
+
+if (file.exists(sqlite_dll_path)) {
+  file.rename(sqlite_dll_path, sqlite_others_path)
+  cat("Renamed System.Data.SQLite.dll to System.Data.SQLite.others.dll\n")
+} else {
+  stop("System.Data.SQLite.dll not found in inst/lib")
+}
