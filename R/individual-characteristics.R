0261604e59473e5c3d16bf99279b5d35cf35a33f
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-02-24 13:04:37 -0500

    Update libs (#1732)
    * update libs
    
    * update libs
    
    * update namespaces
    
    * update namespaces
    
    * - Comment out setting individual parameters in the vignette, until https://github.com/Open-Systems-Pharmacology/OSPSuite-R/issues/1735 is solved
    
    ---------
    
    Co-authored-by: PavelBal <pavel.balazki@gmail.com>

--- R/individual-characteristics.R
+++ R/individual-characteristics.R
@@ -72,7 +72,7 @@ IndividualCharacteristics <- R6::R6Class(
       # or should be initialized automatically
       initPKSim()
       netObject <- rSharp::newObjectFromName(
-        "PKSim.R.Domain.IndividualCharacteristics"
+        "OSPSuite.R.Domain.IndividualCharacteristics"
       )
       super$initialize(netObject)
     },
@@ -115,7 +115,7 @@ IndividualCharacteristics <- R6::R6Class(
         moleculeOntogeny
       )
       netMoleculeOntogeny <- rSharp::newObjectFromName(
-        "PKSim.R.Domain.MoleculeOntogeny"
+        "OSPSuite.R.Domain.MoleculeOntogeny"
       )
       netMoleculeOntogeny$set("Molecule", moleculeOntogeny$molecule)
       netMoleculeOntogeny$set("Ontogeny", moleculeOntogeny$ontogeny)
