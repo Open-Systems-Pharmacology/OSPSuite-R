01862479e92829fcbdfc584a39bbaea3e4b0095d
PavelBal <pavel.balazki@gmail.com>
2026-04-13 17:03:50 +0200

    Re-commit rds for DC and dont create them on test load (failing parallel tests)

--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -1,11 +1,3 @@
 # This file is run once after all tests
 # Clean up watermark option
 options(ospsuite.plots.watermarkEnabled = NULL)
-
-# Remove files created in "create_and_save_data_combined_objects.R"
-# rds_files <- list.files(
-#   testthat::test_path("../data"),
-#   pattern = "\\.rds$",
-#   full.names = TRUE
-# )
-# file.remove(rds_files)
