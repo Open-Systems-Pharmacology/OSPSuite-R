d1880ac80993267f570ae909eff661d3b6985710
Felix MIL <dev@felixmil.com>
2026-04-13 16:15:23 +0200

    Move test setup logic from testthat.R to setup.R
    Recommended practice is to keep testthat.R unmodified and use
    tests/testthat/setup.R for one-time pre-test setup instead.

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,13 +9,4 @@
 library(testthat)
 library(ospsuite)
 
-numCores <- parallel::detectCores()
-numCores <- if (is.na(numCores)) 1L else numCores
-Sys.setenv(TESTTHAT_PARALLEL = "true")
-Sys.setenv(TESTTHAT_CPUS = as.character(numCores))
-
-# Create data combined objects for testing and save them as RDS files. This is done to avoid the need to create these objects from scratch in each test, which can be time-consuming.
-source(file.path("tests", "data", "create_and_save_data_combined_objects.R"))
-
-
 test_check("ospsuite")
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,14 @@
 # This file is run once before all tests
+
 # Set watermark option globally for all tests
 options(ospsuite.plots.watermarkEnabled = TRUE)
+
+# Configure parallel test execution
+numCores <- parallel::detectCores()
+numCores <- if (is.na(numCores)) 1L else numCores
+Sys.setenv(TESTTHAT_PARALLEL = "true")
+Sys.setenv(TESTTHAT_CPUS = as.character(numCores))
+
+# Create data combined objects for testing and save them as RDS files.
+# This avoids recreating these objects from scratch in each test.
+source(testthat::test_path("../data/create_and_save_data_combined_objects.R"))
