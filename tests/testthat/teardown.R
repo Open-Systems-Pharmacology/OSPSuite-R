77fb37ad3c377665140f92dca71d5f09f68a1c4b
PavelBal <pavel.balazki@gmail.com>
2026-04-13 16:53:25 +0200

    Do not remove temp files

--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -3,9 +3,9 @@
 options(ospsuite.plots.watermarkEnabled = NULL)
 
 # Remove files created in "create_and_save_data_combined_objects.R"
-rds_files <- list.files(
-  testthat::test_path("../data"),
-  pattern = "\\.rds$",
-  full.names = TRUE
-)
-file.remove(rds_files)
+# rds_files <- list.files(
+#   testthat::test_path("../data"),
+#   pattern = "\\.rds$",
+#   full.names = TRUE
+# )
+# file.remove(rds_files)
