ccf806138baa944bc770c0a2f75be5c61637579b
PavelBal <pavel.balazki@gmail.com>
2026-04-30 13:23:46 +0200

    Fix test

--- tests/testthat/test-utilities-mobi-module.R
+++ tests/testthat/test-utilities-mobi-module.R
@@ -49,9 +49,7 @@ test_that("loadModuleFromPKML throws an error when the passed PKML contains more
   filePath <- getTestDataFilePath("MoBiProject/TestSim_2Modules.pkml")
   expect_error(
     loadModuleFromPKML(filePath),
-    regexp = "The PKML you are trying to load the module from contains more than one module, but the 
-    function expects only one module.
-    Most probably you are trying to load a simulation export."
+    regexp = "The PKML you are trying to load the module from contains more than one module"
   )
 })
 
