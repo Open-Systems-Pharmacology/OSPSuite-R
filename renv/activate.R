7d7826519951bfb8ad1f83cd36db055cf9f87b6a
PavelBal <pavel.balazki@gmail.com>
2026-05-21 17:39:56 +0200

    Update renv version

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.2.2"
-  attr(version, "md5") <- "bb69b6403b1bad0442657e9e8e57cc83"
+  version <- "1.2.3"
+  attr(version, "md5") <- "59c2d234587132accb321be54c64b7eb"
   attr(version, "sha") <- NULL
 
   # the project directory
